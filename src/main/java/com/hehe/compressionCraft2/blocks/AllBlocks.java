49c562e5bd939106ee703fb0f1e02504b6aa06b2
AlexH <hehe3301@gmail.com>
2016-09-23 17:42:34 -0400

    Textures

--- src/main/java/com/hehe/compressionCraft2/blocks/AllBlocks.java
+++ src/main/java/com/hehe/compressionCraft2/blocks/AllBlocks.java
@@ -1,9 +1,37 @@
 package com.hehe.compressionCraft2.blocks;
 
-public class AllBlocks {
+import java.util.ArrayList;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class AllBlocks {
+	
 	public static void init(){
 		System.out.println("CompressionCraft2 initializing blocks!");
+		ArrayList<Block> blockList =new ArrayList<Block>();
+		
+		for(int i=0; i<8; i++){//Creates Compressed cobble
+			Block block = new Block(Material.ROCK);
+			block.setHardness(1);
+			block.setResistance(9*i);
+			block.setLightLevel(0);
+			//block.setLightOpacity(i);
+			block.setUnlocalizedName("compressedCobble"+i);
+			//block.setCreativeTab(null);
+			blockList.add(block);
+		}
+		
+		
+		
+		for(Block block: blockList){//registers the blocklist
+			
+			GameRegistry.register(block.setRegistryName(block.getUnlocalizedName()));
+			GameRegistry.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+		}
 		
 	}
 }
